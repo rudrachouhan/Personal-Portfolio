9050a47a90e1c6ce27fac5c540959446474d9527
rudrachouhan <rudra678chouhan@gmail.com>
2023-05-17 18:00:36 +0530

    Fixed issues related to responsiveness

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
 import { styles } from "../assets/styles";
 import gitimg from "/github.png";
@@ -21,54 +20,56 @@ const ProjectCard = ({
       variants={fadeIn("right", "spring", index * 0.5, 2)}
       className="cursor-pointer"
     >
-      <Tilt
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
-        className="bg-[#151030] p-5 rounded-2xl sm:w-[360px] w-full"
+      {/* <Tilt
+      options={{
+        max: 45,
+        scale: 1,
+        speed: 450,
+      }}
+      
+    > */}
+    <div className="bg-[#151030] p-5 rounded-2xl sm:w-[360px] w-full">
+      <div
+        className="relative w-full h-[230px]"
+        onClick={() => window.open(link, "_blank")}
       >
-        <div
-          className="relative w-full h-[230px]"
-          onClick={() => window.open(link, "_blank")}
-        >
-          <img
-            src={image}
-            alt="project_image"
-            className="w-full h-full object-cover rounded-2xl cursor-pointer"
-          />
+        <img
+          src={image}
+          alt="project_image"
+          className="w-full h-full object-cover rounded-2xl cursor-pointer"
+        />
 
-          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
-            <div
-              onClick={() => window.open(source_code_link, "_blank")}
-              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
-            >
-              <img
-                src={gitimg}
-                alt="source code"
-                className="w-1/2 h-1/2 object-contain"
-              />
-            </div>
+        <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+          <div
+            onClick={() => window.open(source_code_link, "_blank")}
+            className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+          >
+            <img
+              src={gitimg}
+              alt="source code"
+              className="w-1/2 h-1/2 object-contain"
+            />
           </div>
         </div>
+      </div>
 
-        <div className="mt-5">
-          <h3 className="text-white font-bold text-[24px]">{name}</h3>
-          <p className="mt-2 text-secondary text-[14px]">{description}</p>
-        </div>
+      <div className="mt-5">
+        <h3 className="text-white font-bold text-[24px]">{name}</h3>
+        <p className="mt-2 text-secondary text-[14px]">{description}</p>
+      </div>
 
-        <div className="mt-4 flex flex-wrap gap-2">
-          {tags.map((tag) => (
-            <p
-              key={`${name}-${tag.name}`}
-              className={`text-[14px] ${tag.color}`}
-            >
-              #{tag.name}
-            </p>
-          ))}
-        </div>
-      </Tilt>
+      <div className="mt-4 flex flex-wrap gap-2">
+        {tags.map((tag) => (
+          <p
+            key={`${name}-${tag.name}`}
+            className={`text-[14px] ${tag.color}`}
+          >
+            #{tag.name}
+          </p>
+        ))}
+      </div>
+      {/* </Tilt> */}
+      </div>
     </motion.div>
   );
 };
@@ -90,7 +91,7 @@ const Works = () => {
           <ProjectCard key={`project-${index}`} index={index} {...project} />
         ))}
       </div>
-      </section>
+    </section>
   );
 };
 

9399933415a589f2f0f506674078413b55e7c0bd
rudrachouhan <rudra678chouhan@gmail.com>
2023-05-17 18:22:21 +0530

    resp

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -28,48 +28,49 @@ const ProjectCard = ({
       }}
       
     > */}
-    <div className="bg-[#151030] p-5 rounded-2xl sm:w-[360px] w-full">
-      <div
-        className="relative w-full h-[230px]"
-        onClick={() => window.open(link, "_blank")}
-      >
-        <img
-          src={image}
-          alt="project_image"
-          className="w-full h-full object-cover rounded-2xl cursor-pointer"
-        />
+      <div className="bg-[#151030] p-5 rounded-2xl max-w-[360px] w-full">
+        <div
+          className="relative w-full h-[230px]"
+          onClick={() => window.open(link, "_blank")}
+        >
+          <img
+            src={image}
+            alt="project_image"
+            className="w-full h-full object-cover rounded-2xl cursor-pointer"
+          />
 
-        <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
-          <div
-            onClick={() => window.open(source_code_link, "_blank")}
-            className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
-          >
-            <img
-              src={gitimg}
-              alt="source code"
-              className="w-1/2 h-1/2 object-contain"
-            />
+          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+            <div
+              onClick={() => window.open(source_code_link, "_blank")}
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+            >
+              <img
+                src={gitimg}
+                alt="source code"
+                className="w-1/2 h-1/2 object-contain"
+              />
+            </div>
           </div>
         </div>
+
+        <div className="mt-5">
+          <h3 className="text-white font-bold text-[24px]">{name}</h3>
+          <p className="mt-2 text-secondary text-[14px]">{description}</p>
+        </div>
+
+        <div className="mt-4 flex flex-wrap gap-2">
+          {tags.map((tag) => (
+            <p
+              key={`${name}-${tag.name}`}
+              className={`text-[14px] ${tag.color}`}
+            >
+              #{tag.name}
+            </p>
+          ))}
+        </div>
+        {/* </Tilt> */}
       </div>
 
-      <div className="mt-5">
-        <h3 className="text-white font-bold text-[24px]">{name}</h3>
-        <p className="mt-2 text-secondary text-[14px]">{description}</p>
-      </div>
-
-      <div className="mt-4 flex flex-wrap gap-2">
-        {tags.map((tag) => (
-          <p
-            key={`${name}-${tag.name}`}
-            className={`text-[14px] ${tag.color}`}
-          >
-            #{tag.name}
-          </p>
-        ))}
-      </div>
-      {/* </Tilt> */}
-      </div>
     </motion.div>
   );
 };

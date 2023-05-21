dfc9fd393b2e91c6839a0ed855cfdf4cee905d20
rudrachouhan <rudra678chouhan@gmail.com>
2023-05-22 01:20:17 +0530

    projects section made mobile friendly

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import {Tilt} from "react-tilt";
 import { motion } from "framer-motion";
+
 import { styles } from "../assets/styles";
 import gitimg from "/github.png";
 import { SectionWrapper } from "../assets/hoc";
@@ -20,14 +22,14 @@ const ProjectCard = ({
       variants={fadeIn("right", "spring", index * 0.5, 2)}
       className="cursor-pointer"
     >
-      {/* <Tilt
+      <Tilt
       options={{
         max: 45,
         scale: 1,
         speed: 450,
       }}
       
-    > */}
+    >
       <div className="bg-[#151030] p-5 rounded-2xl max-w-[360px] w-full">
         <div
           className="relative w-full h-[230px]"
@@ -68,10 +70,65 @@ const ProjectCard = ({
             </p>
           ))}
         </div>
-        {/* </Tilt> */}
-      </div>
+        </div>
+        </Tilt>
+      
 
     </motion.div>
+  );
+};
+
+const ProjectCardMobile = ({
+  index,
+  name,
+  description,
+  tags,
+  image,
+  source_code_link,
+  link,
+}) => {
+  return (
+      <div className="bg-[#151030] p-5 rounded-2xl max-w-[360px] w-full">
+        <div
+          className="relative w-full h-[230px]"
+          onClick={() => window.open(link, "_blank")}
+        >
+          <img
+            src={image}
+            alt="project_image"
+            className="w-full h-full object-cover rounded-2xl cursor-pointer"
+          />
+
+          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+            <div
+              onClick={() => window.open(source_code_link, "_blank")}
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+            >
+              <img
+                src={gitimg}
+                alt="source code"
+                className="w-1/2 h-1/2 object-contain"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-5">
+          <h3 className="text-white font-bold text-[24px]">{name}</h3>
+          <p className="mt-2 text-secondary text-[14px]">{description}</p>
+        </div>
+
+        <div className="mt-4 flex flex-wrap gap-2">
+          {tags.map((tag) => (
+            <p
+              key={`${name}-${tag.name}`}
+              className={`text-[14px] ${tag.color}`}
+            >
+              #{tag.name}
+            </p>
+          ))}
+        </div>
+        </div>
   );
 };
 
@@ -80,16 +137,22 @@ const ProjectCard = ({
 const Works = () => {
   return (
     <section className="projects sm:pl-16 pt-12 pb-16 pl-8 sm:pr-0 pr-8" id="projects">
-      <motion.div variants={textVariant()}>
+      <div variants={textVariant()}>
         <p className={`text-white text-2xl`}>
           My <span className="text-[purple]">work</span>{" "}
         </p>
         <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
-      </motion.div>
+      </div>
 
-      <div className="mt-20 flex flex-wrap sm:flex-row flex-col gap-7 items-center">
+      <div className="mt-20 sm:flex flex-wrap sm:flex-row flex-col gap-7 items-center hidden">
         {projects.map((project, index) => (
           <ProjectCard key={`project-${index}`} index={index} {...project} />
+        ))}
+      </div>
+
+      <div className="mt-20 flex flex-wrap sm:flex-row flex-col gap-7 items-center sm:hidden">
+        {projects.map((project, index) => (
+          <ProjectCardMobile key={`project-${index}`} index={index} {...project} />
         ))}
       </div>
     </section>
